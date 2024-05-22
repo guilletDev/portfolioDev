9074f6e5743b6452d696044c17d924a3ce081c4d
guilletdev <guilletdev@gmail.com>
2024-05-22 18:22:54 -0300

    Agrego un provider y modifico el width del bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,12 +14,19 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.3] relative flex items-center justify-center">
+      <div className="h-screen w-[1000px] dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             
         
         
-        
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-[89vw]">
+            <h2>
+                Dynamic Web Magic with Next.js
+            </h2>
+        </div>
+
       </div>
     </div>
   );
