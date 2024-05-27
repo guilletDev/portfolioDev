3f887d9eb3d31afff28ea7884f7fd495ea20a519
guilletdev <guilletdev@gmail.com>
2024-05-27 16:25:06 -0300

    Modifiqué el Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,16 +25,16 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 ">
-                WEB DINÁMICA CON NEXT.JS
+            <h2 className="uppercase tracking-widest text-md md:text-lg text-center text-blue-100 ">
+                ¡Bienvenido a mi Portfolio!
             </h2>
 
             <TextGenerateEffect 
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transformar conceptos en experiencias de usuario perfectas"/>
 
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                Hola, soy Guillermo, Desarrollador Front-end
+            <p className="text-center md:tracking-wider mb-4 text-md md:text-lg lg:text-2xl">
+                ¡Hola! Soy Guillermo, Desarrollador Front-end
             </p>
             <a href="#about">
                 <MagicButton title='Mis Trabajos' icon={<FaLocationArrow />} position="right" />
