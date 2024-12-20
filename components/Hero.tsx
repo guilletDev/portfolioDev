aecaf0a024e3ae11618d2776df9cb6bbb188ecb1
guilletdev <guilletdev@gmail.com>
2024-12-20 15:35:34 -0300

    Modifico el párrafo del Hero y agrego un nuevo proyecto

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { socialMedia } from "@/data";
+import { title } from "process";
 
 
 const Hero = () => {
@@ -27,13 +28,15 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widest text-md md:text-lg text-center text-blue-100 ">
-                ¡Bienvenido a mi Portfolio!
-            </h2>
+            
 
             <TextGenerateEffect 
             className="text-center text-5xl md:text-5xl lg:text-6xl"
             words="¡Hola! Soy Guillermo, Desarrollador Front-end"/>
+            <h2 className="text-md text-center text-blue-100 text-lg ">
+            
+            </h2>
+            <p className="text-white-200 md:text-lg my-5 text-center font-medium roboto">Analista de Sistemas y Desarrollador web Frontend, enfocado en construir experiencias digitales que aporten valor y conecten con las personas </p>
             
 
             {/* <p className="text-center md:tracking-wider mb-4 mt-1 text-lg md:text-lg lg:text-2xl">
@@ -45,10 +48,11 @@ const Hero = () => {
 
             <div className="flex items-center md:gap-3 gap-4 mt-5 md:mt-7">
                     {socialMedia.map((profile)=>(
-                        <div key={profile.id} className="flex w-10 h-10 cursor-pointer justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200
+                        <div key={profile.id} className="flex md:w-28 h-10 cursor-pointer justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200
                         rounded-lg border border-black-300">
-                            <a target="blank" href={profile.link} className="flex gap-2">
+                            <a target="blank" href={profile.link} className="flex gap-2 justify-center">
                                 <img src={profile.img} alt='Iconos del footer' height={20} width={20} />
+                                <h3>{profile.title}</h3>
                                 
                             </a>
                             
